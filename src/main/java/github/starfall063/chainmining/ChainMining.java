47fd07a2c5179f4f1c54f2a134e2eaa5b4cca475
StarFall063 <liuyzheng418@163.com>
2026-07-08 15:59:08 +0800

    test

--- src/main/java/github/starfall063/chainmining/ChainMining.java
+++ src/main/java/github/starfall063/chainmining/ChainMining.java
@@ -9,13 +9,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ChainMining.MOD_ID, name = ChainMining.MOD_NAME, version = ChainMining.VERSION, guiFactory = "github.starfall063.chainmining.config.ChainMiningGuiFactory")
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class ChainMining {
-    public static final String MOD_ID = "chainmining";
-    public static final String MOD_NAME = "Chain Mining";
-    public static final String VERSION = "1.0.0";
-
-    public static final Logger logger = LogManager.getLogger(MOD_NAME);
+    public static final Logger logger = LogManager.getLogger(Tags.MOD_NAME);
 
     @SidedProxy(
             clientSide = "github.starfall063.chainmining.proxy.ClientProxy",
